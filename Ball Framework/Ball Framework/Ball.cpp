f8d046910ebadd224243b8cb0776cd00f2c410d1
marius_gociman <mariusgociman74@yahoo.it>
2020-11-20 22:41:46 +0200

    Added Getters and Setters for Ball class

--- Ball Framework/Ball Framework/Ball.cpp
+++ Ball Framework/Ball Framework/Ball.cpp
@@ -9,3 +9,55 @@ Ball::Ball(const Vector2& position, float size, const Vector2& direction, float 
 	: m_position(position), m_size(size), m_direction(direction), m_speed(speed)
 {
 }
+
+Vector2 Ball::getPosition()
+{
+	return m_position;
+}
+
+float Ball::getSize()
+{
+	return m_size;
+}
+
+Vector2 Ball::getDirection()
+{
+	return m_direction;
+}
+
+float Ball::getSpeed()
+{
+	return m_speed;
+}
+
+void Ball::setPoisiton(const Vector2& position)
+{
+	m_position = position;
+}
+
+void Ball::setPosition(float x, float y)
+{
+	m_position.setX(x);
+	m_position.setY(y);
+}
+
+void Ball::setSize(float size)
+{
+	m_size = size;
+}
+
+void Ball::setDirection(const Vector2& direction)
+{
+	m_direction = direction;
+}
+
+void Ball::setDirection(float x, float y)
+{
+	m_direction.setX(x);
+	m_direction.setY(y);
+}
+
+void Ball::setSpeed(float speed)
+{
+	m_speed = speed;
+}
--- Ball Framework/Ball Framework/Ball.h
+++ Ball Framework/Ball Framework/Ball.h
@@ -7,6 +7,19 @@ public:
 	Ball();
 	Ball(const Vector2& position, float size, const Vector2& direction, float speed);
 
+	Vector2 getPosition();
+	float getSize();
+	Vector2 getDirection();
+	float getSpeed();
+
+	void setPoisiton(const Vector2& position);
+	void setPosition(float x, float y);
+	void setSize(float size);
+	void setDirection(const Vector2& direction);
+	void setDirection(float, float y);
+	void setSpeed(float speed);
+
+
 private:
 	Vector2 m_position, m_direction;
 	float m_size, m_speed;
