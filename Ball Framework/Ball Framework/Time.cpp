48d223286a108997fbf34708ca0b256e96c94a66
Andrei Maroiu <achimaroiu@yahoo.com>
2020-11-20 18:54:37 +0200

    Updated Time class to support both locked and unlocked frame rate

--- Ball Framework/Ball Framework/Time.cpp
+++ Ball Framework/Ball Framework/Time.cpp
@@ -1,20 +1,50 @@
 #include "Time.h"
 
 Time::TimeAux::TimeAux()
-	: deltaTime{ 0.0 }, realTime{ 0.0 }, now{ std::chrono::system_clock::now() },
+	: deltaTime{ 0.0 }, realTime{ 0.0 }, realDelta{ 0.0 }, timeSinceStart{ 0.0 },
+	now{ std::chrono::system_clock::now() },
 	last{ std::chrono::system_clock::now() } {}
 
+void Time::TimeAux::resetTime()
+{
+	realTime = 0.0;
+	deltaTime = 0.0;
+	realDelta = 0.0;
+}
+
 Time::TimeAux Time::aux;
+
+void Time::resetTime()
+{
+	aux.resetTime();
+}
 
 void Time::updateTime()
 {
 	aux.now = std::chrono::system_clock::now();
 
 	std::chrono::duration<double> dif = aux.now - aux.last;
-	aux.deltaTime = dif.count();
-	aux.realTime += aux.deltaTime;
+	aux.realDelta = dif.count();
+	aux.deltaTime += aux.realDelta;
+	aux.realTime += aux.realDelta;
+	aux.timeSinceStart += aux.realDelta;
 
 	aux.last = aux.now;
+}
+
+void Time::updateDone()
+{
+	aux.deltaTime = 0.0;
+}
+
+double Time::getRealDelta()
+{
+	return aux.realDelta;
+}
+
+double Time::getTimeSinceStartUp()
+{
+	return aux.timeSinceStart;
 }
 
 double Time::getDeltaTime()
--- Ball Framework/Ball Framework/Time.h
+++ Ball Framework/Ball Framework/Time.h
@@ -8,11 +8,15 @@ private:
 	{
 	private:
 		double deltaTime;
+		double realDelta;
+		double timeSinceStart;
 		double realTime;
 		std::chrono::time_point<std::chrono::system_clock> last;
 		std::chrono::time_point<std::chrono::system_clock> now;
 
 		TimeAux();
+
+		void resetTime();
 
 	public:
 		TimeAux(const TimeAux&) = delete;
@@ -29,7 +33,11 @@ public:
 	Time(const Time&) = delete;
 	Time& operator=(const Time&) = delete;
 
+	static void resetTime();
 	static void updateTime();
+	static void updateDone();
+	static double getRealDelta();
+	static double getTimeSinceStartUp();
 	static double getDeltaTime();
 	static double getRealTime();
 };
