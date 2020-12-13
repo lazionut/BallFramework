2b0b9494911b546ffdf59bdd83294ac4ffe6ca88
Daniela <sirpurple2016@yahoo.com>
2020-12-13 15:44:15 +0200

    Updated PlayersStatistics class

--- Ball Framework/Ball Framework/PlayersStatistics.cpp
+++ Ball Framework/Ball Framework/PlayersStatistics.cpp
@@ -53,3 +53,46 @@ void PlayersStatistics::ReadStatistics(const std::string& inFile)
 		throw "File could not be opened ";
 	fin.close();
 }
+
+void Swap(std::vector<std::variant<std::string, uint16_t>>* first, std::vector<std::variant<std::string, uint16_t>>* second)
+{
+	std::vector<std::variant<std::string, uint16_t>>* temp;
+	temp = first;
+	first = second;
+	second = temp;
+
+}
+void OrderStatistiscs(std::vector<std::vector< std::variant <std::string, uint16_t> > >& statistics)
+{
+
+	for (uint16_t index = 0; index < statistics.size() - 1; index++)
+	{
+		for (uint16_t index2 = index; index2 < statistics.size(); index2++)
+		{
+			uint16_t firstScore = std::get<uint16_t>(statistics[index][2]) - std::get<uint16_t>(statistics[index][3]);
+			uint16_t secondScore = std::get<uint16_t>(statistics[index2][2]) - std::get<uint16_t>(statistics[index2][3]);
+
+			if (firstScore < secondScore)
+			{
+				Swap(&statistics[index], &statistics[index2]);
+			}
+		}
+	}
+
+}
+
+std::ostream& operator<<(std::ostream& outStream, PlayersStatistics& other)
+{
+
+
+	OrderStatistiscs(other.m_statistics);
+
+	for (uint16_t index = 0; index < other.m_noPlayers; index++)
+	{
+		outStream << std::get<std::string>(other.m_statistics[index][0]) << " ";
+		outStream << std::get<std::uint16_t>(other.m_statistics[index][1]) << " ";
+		outStream << std::get<std::uint16_t>(other.m_statistics[index][2]) << " ";
+		outStream << std::get<std::uint16_t>(other.m_statistics[index][3]) << std::endl;
+	}
+	return outStream;
+}
--- Ball Framework/Ball Framework/PlayersStatistics.h
+++ Ball Framework/Ball Framework/PlayersStatistics.h
@@ -12,6 +12,11 @@ public:
 	PlayersStatistics(uint16_t noPlayers);
 	void ReadStatistics(const std::string& inFile);
 
+
+	friend void Swap(std::vector < std::variant <std::string, uint16_t> >* first, std::vector < std::variant <std::string, uint16_t> >* second);
+	friend void OrderStatistiscs(std::vector<std::vector< std::variant <std::string, uint16_t> > >& statistics);
+
+	friend std::ostream& operator<<(std::ostream& outStream, PlayersStatistics& other);
 private:
 
 	std::vector<std::vector< std::variant <std::string, uint16_t> > > m_statistics;
