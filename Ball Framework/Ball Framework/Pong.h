d69d9d1a017a5c45d070cd6c9650546c240d08ca
lazionut <lionut@mailbox.org>
2020-12-23 19:47:11 +0200

    Refactored BrickBreaker and Pong classes

--- Ball Framework/Ball Framework/Pong.h
+++ Ball Framework/Ball Framework/Pong.h
@@ -1,33 +1,32 @@
 #pragma once
-#include "Game.h"
-#include "Ball.h"
-#include "Paddle.h"
-#include "Score.h"
-#include "ScreenScale.h"
-#include "PickUpGenerator.h"
-#include "Button.h"
 #include <SDL_ttf.h>
-#include <vector>
+#include "Game.h"
+#include "ScreenScale.h"
+#include "Paddle.h"
+#include "Ball.h"
+#include "PickUpGenerator.h"
+#include "Score.h"
+#include "Button.h"
 
 class Pong : public Game
 {
 public:
 	Pong(uint16_t width, uint16_t height, TTF_Font* font, uint32_t flags = 0, uint16_t maxFPS = 0);
 
-private:
+private: //override Game class methods
 	void Start() override;
+	void Update() override;
 	void OnClose() override;
 	void CheckCollision() override;
-	void Update() override;
 	void KeyPressed(const SDL_Keycode& key) override;
 	void KeyReleased(const SDL_Keycode& key) override;
 	void MousePressed(const SDL_MouseButtonEvent& mouse) override;
 	void MouseReleased(const SDL_MouseButtonEvent& mouse) override;
-	int IsInBounds(Sint32 x, Sint32 y);
 	void Render(SDL_Renderer* renderer) override;
 
-	void CheckBallWallCollision();
+private: //class methods
 	void CheckPaddleWallCollision();
+	void CheckBallWallCollision();
 	void CheckBallPaddleCollision();
 	void CheckBallBrickCollision();
 	void CheckPickUpCollision();
@@ -35,36 +34,42 @@ private:
 
 	void InitializeBricks();
 	void RenderBricks(SDL_Renderer* renderer);
-	void RenderPlayersScore(SDL_Renderer* renderer);
-	void Pause();
-	void ResetBall();
-	void CreatePickUp(const Vector2& position);
-	void RenderButton(SDL_Renderer* renderer);
 
-	Paddle m_pongPaddle1;
-	Paddle m_pongPaddle2;
+	void CreatePickUp(const Vector2& position);
+
+	void RenderPlayersScore(SDL_Renderer* renderer);
+
+	void RenderButton(SDL_Renderer* renderer);
+	int IsInBounds(Sint32 x, Sint32 y);
+	void Pause();
+
+private: //SDL objects
+	SDL_Renderer* m_renderer;
+	SDL_Texture* m_ballImage;
+	SDL_Texture* m_pickUpImage;
+	TTF_Font* m_buttonFont;
+
+	Paddle m_paddlePlayer1;
+	Paddle m_paddlePlayer2;
+
+private: //objects
+	Ball m_ball;
+
 	std::vector<std::vector<Rectangle>> m_bricks;
 	uint16_t m_bricksNumber;
 
-	Ball m_pongBall;
-	SDL_Texture* m_ballImage;
-	SDL_Renderer* m_renderer;
-
-	TTF_Font* m_font;
-
-	Score m_pongScore1;
-	Score m_pongScore2;
-
 	PickUpGenerator m_pickUpGenerator;
-
 	PickUp m_pickUp;
-	SDL_Texture* m_pickUpImage;
+
+	Score m_scorePlayer1;
+	Score m_scorePlayer2;
 
 	Button m_pauseButton;
 
-	float m_lastTimeScale;
-
-	bool m_paused;
-	bool m_isPickActive;
+private: //primitive types
 	bool m_isPickCreated;
+	bool m_isPickActive;
+
+	float m_lastTimeScale;
+	bool m_isPaused;
 };
