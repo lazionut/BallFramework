df0f1e1c910750e180c22e6ebb4b3973040b0420
Daniela <sirpurple2016@yahoo.com>
2020-12-27 18:47:53 +0200

    Updated Paths class

--- Ball Framework/Ball Framework/Paths.cpp
+++ Ball Framework/Ball Framework/Paths.cpp
@@ -30,4 +30,26 @@ Paths::Paths()
 
 void Paths::ReadPaths()
 {
+	std::ifstream fin(m_filePath);
+
+	if (fin.is_open() && m_filePath != "")
+	{
+		if (fin.peek() == std::ifstream::traits_type::eof())
+		{
+			m_numberOfAssets = 0;
+			return;
+		}
+		fin >> m_numberOfAssets;
+		std::string objectKey, path;
+		for (auto index = 0; index < m_numberOfAssets; index++)
+		{
+			fin >> objectKey;
+			fin >> path;
+			m_assetsPaths[objectKey] = path;
+		}
+		fin.close();
+	}
+	else {
+		std::cout << "Could not open file!";
+	}
 }
